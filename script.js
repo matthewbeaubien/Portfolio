fbd0dde10a584cafa1541f2a62c5bb567206b76e
Matthew Beaubien <matthewabeaubien@gmail.com>
2020-04-25 23:30:28 -0400

    Redesign

---
+++ script.js
@@ -0,0 +1,73 @@
+
+function display1() {
+  var x = document.getElementById("project1");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function display2() {
+  var x = document.getElementById("project2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function display3() {
+  var x = document.getElementById("project3");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function display4() {
+  var x = document.getElementById("project4");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function display5() {
+  var x = document.getElementById("project5");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function display6() {
+  var x = document.getElementById("project6");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function display7() {
+  var x = document.getElementById("project7");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+
+
+function mouseOver() {
+  document.getElementById("about").style.display = "block";
+}
+
+function mouseOut() {
+  document.getElementById("about").style.display = "none";
+}
+
